7cd493aca8b062fa40bb76626121532322514bc5
hareem Yashal <hareem@ccript.com>
2025-10-23 19:44:30 +0500

    Update dialog.jsx

--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -32,7 +32,7 @@ const DialogContent = React.forwardRef(
         ref={ref}
         className={cn(
           // layout + height + scroll
-          "fixed left-1/2 top-1/2 z-50 grid w-[90%] max-w-lg max-h-[96vh] translate-x-[-50%] translate-y-[-50%] overflow-y-auto rounded-2xl border bg-white p-5 sm:p-6 shadow-lg duration-200",
+          "fixed left-1/2 top-1/2 z-50 grid w-[90%] max-w-lg max-h-[90vh] translate-x-[-50%] translate-y-[-50%] overflow-y-auto rounded-2xl border bg-white p-5 sm:p-6 shadow-lg duration-200",
 
           // animations
           "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
